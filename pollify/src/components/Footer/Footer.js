5ccdae3c4f55a84b37ec268d3381dfbe230a2b0d
MsMalcolm <70501694+MsMalcolm@users.noreply.github.com>
2020-09-27 15:44:24 -0400

    Upload of the header and footer

---
+++ pollify/src/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './footer.css';
+
+export default function Footer(){
+    return(
+        <footer> &#169; 2020 Pollify Co. All Rights Reserved</footer>
+    );
+}
---
+++ pollify/src/components/Navigation/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './nav.css';
+
+export default class Navbar extends React.Component{
+    
+    state = {open: false};
+
+    openMenu = () => {
+        this.setState({open: !this.state.open})
+    }
+
+    render(){
+
+        return(
+            <nav>
+                <label class="logo"> 
+                    <span>P</span>ollify 
+                    <img src="icon.png" alt="Free Logo Design" id="icon" />
+                </label>
+                <ul class = {this.state.open ? "showMenu" : "hideMenu"}>
+                    <li><a class="active" href="#"> Home </a></li>
+                    <li><a href="#"> Login/Signup </a></li>
+                    <li><a href="#"> Make A Poll </a></li>
+                </ul>
+                <div class = 'hamburger-menu' onClick = {this.openMenu}>
+                    <i class= {this.state.open ? "fas fa-times" : "fas fa-bars"}></i>
+                </div>
+            </nav>
+        );
+    }
+}
