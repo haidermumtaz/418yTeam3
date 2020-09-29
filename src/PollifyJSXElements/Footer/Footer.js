e116b3dabc9b22aee2c58e288e9b336f20f33b69
zwmillerUA <zwmiller@albany.edu>
2020-09-28 22:20:21 -0400

    pollmenu first push

---
+++ src/PollifyJSXElements/Footer/Footer.js
@@ -0,0 +1,10 @@
+/* Shaniya Malcolm's Pollify Footer JSX Element */
+
+import React from 'react';
+import './footer.css';
+
+export default function Footer(){
+    return(
+        <footer> &#169; 2020 Pollify Co. All Rights Reserved</footer>
+    );
+}
