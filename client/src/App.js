d3b7dd22cc020d3e01c7578869aa51bbbfae6ee9
Rob McIlrath <romcilrath@gmail.com>
2020-10-19 19:22:08 -0400

    Okay something is working at least now

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navigation/Navbar'
 import Footer from './components/Footer/Footer'
+import Login from './pages/Login/Login'
+import Signup from './pages/Signup/Signup'
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Signup />
       <Footer />
     </div>
   );
