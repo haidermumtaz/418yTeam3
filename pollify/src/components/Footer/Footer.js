4d204c0de4ac3e227dd3019ff8b205d18488a9dd
Rob McIlrath <romcilrath@gmail.com>
2020-10-19 13:50:05 -0400

    Starting merge of login front/backend

--- pollify/src/components/Footer/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './footer.css';
-
-export default function Footer(){
-    return(
-        <footer> &#169; 2020 Pollify Co. All Rights Reserved</footer>
-    );
-}
