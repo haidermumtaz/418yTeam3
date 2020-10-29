f9ccd271c91b48852847f9e596b0974928cac2ff
Shaniya <shaniyasmalcolm@gmail.com>
2020-10-28 23:21:19 -0400

    Merging landing_page into Frontend

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {BrowserRouter, Route} from 'react-router-dom'
+import Home from './pages/Landing_Page/Home';
+import Login from './pages/Login/Login';
+import Signup from "./pages/Signup/Signup"
+
+export default class App extends Component {
+  render(){
+    return (
+      <BrowserRouter>
+        <Route path = "/" component={Home} exact= {true}  />
+        <Route path = "/login" component={Login} />
+        <Route path = "/register" component={Signup}/>
+        <Route path = "/polls" />
+      </BrowserRouter>
+    );
+  }
+}
---
+++ client/src/pages/Signup/Signup.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import NavBar from '../../components/Navigation/Navbar'
+import Footer from '../../components/Footer/Footer'
+import './signup.css';
+
+export default function Signup(){
+    return(
+        <div className = 'registrationpage'>
+            <NavBar />
+            <div class="signupbox">
+                <div className = 'signup'>
+                    <h1>SIGN UP</h1>
+                </div>
+                <form>
+                    <p>Email</p>
+                    <input type="text" name = "" placeholder = "Enter Email"/>
+                    <p>Username</p>
+                    <input type="text" name = "" placeholder = "Enter Username"/>
+                    <p>Password</p>
+                    <input type = "password" name = "" placeholder="Enter Password"/>
+                    <p>Confirm Password</p>
+                    <input type = "password" name = "" placeholder="Re-enter Password"/>
+                    <p className = 'errorMessage'>ERROR: Passwords do not match. Try typing again.</p>
+                    <input type = "submit" name="" value="Register"/>
+                    <a href="/login">Already have an account? Log in here!</a>
+                </form>
+            </div>
+            <Footer />
+        </div>
+    )
+}
