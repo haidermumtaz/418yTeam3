17228e5300b34f1fe209edcaa7a055bb9ea0a916
Shaniya <shaniyasmalcolm@gmail.com>
2020-11-08 12:21:44 -0500

    Updated Navbar
    - Made navbar using react-bootstrap
    - Deleted css file
    - New icon in public folder
    - Changed Navbar.js to Navigation.js because the name conflicted with my imports.

---
+++ client/src/components/Navigation/Navigation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Nav} from 'react-bootstrap'
+
+export default class Navigation extends React.Component{
+
+     render(){
+
+        return(
+            <div>
+                <Navbar collapseOnSelect expand="sm" bg="#2c3e50" variant="dark">
+                <Navbar.Brand style={{fontSize: 30}} href="/">
+                    Pollify
+                    <img src="icon4.png" alt=" " id="icon" style={{height: 40, marginLeft:10, marginBottom: 10}}/>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto" />
+                    <Nav>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link eventKey={2} href="/login">Login</Nav.Link>
+                        <Nav.Link eventKey={3} href="/polls">Create Poll</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                </Navbar>
+            </div>
+        );
+     }
+}
