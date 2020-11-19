54531cdd84145c11ee9bbac1785c18f30af8227d
Shaniya <shaniyasmalcolm@gmail.com>
2020-11-19 16:36:34 -0500

    Updated the homepage
    Changed video tutorial into a features section

--- client/src/pages/Landing_Page/Home.js
+++ client/src/pages/Landing_Page/Home.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import './home.css';
-import NavBar from '../../components/Navigation/Navigation'
-import Footer from '../../components/Footer/Footer'
+import Footer from '../../components/Footer/Footer';
+import { Col, Container, Row } from 'react-bootstrap';
 
 export default function Home(){
     return(
         <div className = "home-page">
-            {/* <NavBar /> */}
             <section class="home-intro">
                 <div class="welcome">
-                    <h1>Welcome to Pollify!</h1>
+                    <h1>Welcome to <span>P</span>ollify!</h1>
                 </div>
             </section>
-            <section class="howto">
-                <div class="tutorial">
-                    <p><strong> Watch this tutorial on how to make a poll! </strong></p>
-                    <iframe width="420" height="315"
-                    src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1">
-                    Your browser does not support the video tag.
-                    </iframe>    
-                </div>
-            </section>
+            <Container className = "features">
+                    
+                <h1><strong>Features</strong></h1>
+                
+                <Row>
+                    <Col className = 'box'>
+                       <h4>VIEW POLLS</h4> 
+                       <p>View polls you created and see what people voted on!</p>
+                    </Col>
+                    <Col className = 'box'>
+                        <h4>MAKE A POLL</h4>
+                        <p>Easy and simple ways to design and share polls!</p>
+                    </Col>
+                    <Col className = 'box'>
+                        <h4>FREE AND SAFE</h4>
+                        <p>Creating a poll is 100% FREE! Pollify ensures your data will be safe!</p>
+                    </Col>
+                </Row>
+            </Container>
             <Footer />
         </div>
     );
