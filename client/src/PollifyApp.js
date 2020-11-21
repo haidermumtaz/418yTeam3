5ed6cf00173da9d7547b56eef24551726542ac54
zwmillerUA <zwmiller@albany.edu>
2020-11-21 16:07:23 -0500

    added my pages to merged frontend

--- client/src/PollifyApp.js
+++ client/src/PollifyApp.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import MyPollsMenu from './pages/MyPollsMenu';
 import PollCreation from './pages/PollCreation';
 import Footer from './components/Footer/Footer';
-import PollifyNavbar from './components/Navigation/Navbar';
+import PollifyNavbar from './components/Navigation/Navigation';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
@@ -14,9 +14,9 @@ import {Route, Link, BrowserRouter as PollifyRouter} from 'react-router-dom';
 export default function PollifyApp(){
     return (
         <PollifyRouter>
-            <div style={{backgroundImage: `url("https://wallpaperaccess.com/full/1682999.jpg")`}}>
+            <PollifyNavbar/>
+            <Container fluid style={{backgroundImage: `url("https://wallpaperaccess.com/full/1682999.jpg")`}}>
                 <Row>
-                    <PollifyNavbar/>
                     <Route path="/mypolls">
                         <MyPollsMenu/>
                     </Route>
@@ -28,7 +28,7 @@ export default function PollifyApp(){
                     </Route>
                     <Footer/>
                 </Row>
-            </div>
+            </Container>
         </PollifyRouter>    
     )
 }
