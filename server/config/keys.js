6b36bd39d0bb9f1387e6c0eadaccf15f5de04b9b
Robert McIlrath <romcilrath@gmail.com>
2020-11-22 16:22:09 -0500

    Comments

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,3 +1,8 @@
+// Robert McIlrath
+// References code by Rishi Prasad from tutorial: https://blog.bitsrc.io/build-a-login-auth-app-with-mern-stack-part-1-c405048e3669
+
+// Configures key for export by connecting to my MongoDB cluster (Cluster #0) via my mongoURI
+
 module.exports = {
 	mongoURI: "mongodb+srv://rob_mcilrath:passwordbaby@cluster0.unnyg.mongodb.net/Cluster0?retryWrites=true&w=majority",
 	secretOrKey: "secret"
