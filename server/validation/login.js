4aefd5216dcceb3cef04000239899051e4afbe70
Robert McIlrath <romcilrath@gmail.com>
2020-11-22 16:36:44 -0500

    Comments
    Added username check too

--- server/validation/login.js
+++ server/validation/login.js
@@ -1,4 +1,11 @@
-//Rob McIlrath + Bianca Lavaud
+// Robert McIlrath
+// References code by Rishi Prasad from tutorial: https://blog.bitsrc.io/build-a-login-auth-app-with-mern-stack-part-1-c405048e3669
+// Most modifications to code were to add username concept, other modifications were minor
+
+// Checks that the email isn't empty and is valid
+// Checks the password isn't empty
+// Checks the username isn't empty
+
 const Validator = require("validator");
 const isEmpty = require("is-empty");
 
@@ -7,6 +14,7 @@ module.exports = function validateLoginInput(data) {
   
 // Convert empty fields to an empty string so we can use validator functions
   data.email = !isEmpty(data.email) ? data.email : "";
+  data.username = !isEmpty(data.username) ? data.username : "";
   data.password = !isEmpty(data.password) ? data.password : "";
   
 // Email checks
@@ -16,7 +24,12 @@ module.exports = function validateLoginInput(data) {
     errors.email = "Email is invalid";
   }
   
-// Password checks
+  // Username check
+  if (Validator.isEmpty(data.username)) {
+    errors.password = "Username field is required";
+  }
+  
+// Password check
   if (Validator.isEmpty(data.password)) {
     errors.password = "Password field is required";
   }
