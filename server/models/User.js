442df9b8129a1ad86fe1802aa22d13d4a2ddcdf6
Robert McIlrath <romcilrath@gmail.com>
2020-11-22 16:32:17 -0500

    Comments

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,12 @@
-//Bianca Lavaud
+// Bianca Lavaud (helped with defining user structure)
+// Robert McIlrath
+// References code by Rishi Prasad from tutorial: https://blog.bitsrc.io/build-a-login-auth-app-with-mern-stack-part-1-c405048e3669
+// Most modifications to code were to require a unique username/ add username concept, other modifications were minor
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// Create Schema
+// Create User schema, requiring username, email, password and creation date (passes current date)
 const UserSchema = new Schema({
   username: {
     type: String,
